a45a415540b82bd80beb244b8cf173f080e1c9d3
chaser114 <z13672872031@qq.com>
2025-07-28 19:04:41 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,6 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "chaser的博客",
-	subtitle: "",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th', 'vi'
 	themeColor: {
 		hue: 235, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
